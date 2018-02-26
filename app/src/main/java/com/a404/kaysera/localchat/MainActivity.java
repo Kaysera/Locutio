faf70685cc7e3ad04ba103eefbb8e99a98a85b9a
Guillermo Fernández <GuillermoTomas.Fernandez@alu.uclm.es>
2018-02-26 21:41:52 +0100

    Initial Commit

---
+++ app/src/main/java/com/a404/kaysera/localchat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a404.kaysera.localchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Esto es un comment de pruebita
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
